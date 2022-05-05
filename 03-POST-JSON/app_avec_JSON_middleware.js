72d4d5ff0b7acc4283e33e596530f2a6c228b00b
Faez-B <faezbhatti@gmail.com>
2022-05-05 13:20:45 +0200

    Test comportement requete POST avec et sans middleware.

---
+++ 03-POST-JSON/app_avec_JSON_middleware.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // middleware
+
+app.post('/', (req, res) => {
+	console.log(req.body);
+	res.send(req.body);
+})
+
+app.listen(3000);
+
+// Tester : http POST localhost:3000 hello=World valeur=2
